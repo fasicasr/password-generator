67549c2609493ae0f63d7c637284324bc01c7d4e
Fasica Robi <fasicasr@gmail.com>
2021-03-06 23:30:06 -0500

    second commit

--- Develop/script.js
+++ Develop/script.js
@@ -10,13 +10,57 @@ let confirmLower;
 let confirmUpper;
 let confirmNumber;
 let confirmSpecial;
-let userChoices = [];
+
 
 let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
+function generatePassword() {
+  let userChoices = '';
+  let result = '';
+  //While loop to make sure criteria length is met
+ while(1) {
+    passwordLength = prompt("Indicate the number of characters for your password? Please choose between 8 and 128");
+    if(passwordLength === null){
+      return result;
+    }
+    if(!passwordLength) {
+      alert("Character length required");
+
+    } else if (passwordLength < 8 || passwordLength > 128) {
+      passwordLength = alert("Password must be 8-128 characters long")
+
+    } else {
+      break;
+    }
+  }
+  //select character types - at least one has to be selected to move forward
+  confirmUpper = confirm("Would you like to add upper case letters?");
+  confirmLower = confirm("Would you like to add lower case letters?");
+  confirmNumber = confirm("Would you like to add numbers?");
+  confirmSpecial = confirm("Would you like to add special characters?");
+  
+  if (confirmUpper) {
+    userChoices += upperCase.join('');
+  }
+  if (confirmLower) {
+    userChoices += lowerCase.join('');
+  }
+  if (confirmNumber) {
+     userChoices+= numbers.join('');
+  }
+  if (confirmSpecial) {
+    userChoices += special.join('');
+  }
+   //for loop to generate random password with the chosen character types by user
+  for(let i = 0; i < passwordLength; i++){
+    result += userChoices.charAt(Math.floor(Math.random() * userChoices.length));
+  }
+
+  return result;
+}
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
@@ -24,45 +68,8 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function generatePassword() {
-  //Alerts (pop up) when user doesn't choose character amount or the amount is under 8 or above 123 characters
-  passwordLength = prompt("Indicate the number of characters for your password? Please choose between 8 and 128");
 
-  if(!passwordLength) {
-    alert("Character length required");
 
-  } else if (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("Password must be 8-128 characters long")
-  
-  } else { 
-    confirmLower = confirm("Would you like to add lower case letters?");
-    confirmUpper = confirm("Would you like to add upper case letters?");
-    confirmNumber = confirm("Would you like to add numbers?");
-    confirmSpecial = confirm("Would you like to add special characters?");
-  };
-
-  if (confirmUpper) {
-    userChoices.push(upperCase);
-  }
-  if (confirmLower) {
-    userChoices.push(lowerCase);
-  }
-  if (confirmNumber) {
-    userChoices.push(numbers);
-  }
-  if (confirmSpecial) {
-    userChoices.push(special);
-  }
-
-  return userChoices.join('');
-}
-//   // Loop for random selection
-//   for (let i = 0; i < passwordLength; i++) {
-//     generatePassword.push(userChoices[(Math.floor(Math.random()*userChoices.length))]);
-//   }
-  
-//   return (generatePassword.join(''));
-// } 
 
   
 
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# password-generator
+# sassy-password-generator
+
+Sassy password generator is an application that enables a user to generate random passwords based on criteria selected by the user. The password can include upper case, lowercase, numbers, and special characters. 
+
+# Criteria
+
+* User must indicate password length of at least 8 characters and no more than 128 characters
+* User must select at least one (lowercase, uppercase, numeric, and/or special characters) when generating password
+
+# Webpage 
+
+*Sassy Password Generator 
+![webpage image](./Assets/spg-webpage-screenshot.png)
+
+
+
+Application URL: https://fasicasr.github.io/sassy-password-generator/Develop/index.html
+Github Repository: https://github.com/fasicasr/sassy-password-generator.git
